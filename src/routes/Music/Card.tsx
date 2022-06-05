b97511d55bff155c8d73df0e76006fc8034b681d
유승완 <51105841+Seung-wan@users.noreply.github.com>
2022-06-05 11:56:12 +0900

    change state to recoil

--- src/routes/Music/Card.tsx
+++ src/routes/Music/Card.tsx
@@ -1,6 +1,8 @@
 import { MouseEvent } from 'react'
 
 import styles from './card.module.scss'
+import { useSetRecoilState } from 'recoil'
+import { countState, correctState, wrongState } from '../recoil/music'
 
 interface Props {
   categoryTitle: string
@@ -9,15 +11,23 @@ interface Props {
 }
 
 const Card = ({ categoryTitle, image, setCategory }: Props) => {
-  const clickHandler = (evt: MouseEvent<HTMLButtonElement>) => {
+  const setCount = useSetRecoilState(countState)
+  const setCorrect = useSetRecoilState(correctState)
+  const setWrong = useSetRecoilState(wrongState)
+
+  const handleClickButton = (evt: MouseEvent<HTMLButtonElement>) => {
+    setCount(0)
+    setCorrect(false)
+    setWrong(false)
+
     const { title } = evt.currentTarget.dataset
     const convertTitle = title?.slice(14).match(/^i\d+/i)
     if (convertTitle) setCategory(convertTitle[0])
   }
   return (
     <div className={styles.container}>
-      <h2>{categoryTitle}</h2>
-      <button type='button' data-title={image} onClick={clickHandler}>
+      <h2 className={styles.categoryTitle}>{categoryTitle}</h2>
+      <button type='button' data-title={image} onClick={handleClickButton}>
         <img className={styles.image} src={image} alt='singer' />
       </button>
     </div>
